0414025a906d80d08b13bcddacc4263e9aabb6e5
judgeOstrich <jeraldnyaga15@gmail.com>
2025-03-19 04:23:43 +0300

    Add functions

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(val){
+    return Math.abs(parseInt(val,10) - 42)
+}
+
+function distanceFromHqInFeet(val){
+    return parseInt(distanceFromHqInBlocks(val) * 264)
+}
+
+function distanceTravelledInFeet(start, end){
+    return parseInt(Math.abs(end - start) * 264, 10)
+}
+
+function calculatesFarePrice(start, destination){
+    const distanceFeet  = distanceTravelledInFeet(start, destination);
+
+    if(distanceFeet <= 400){
+        return 0
+    } else if(distanceFeet > 400 && distanceFeet <= 2000){
+        return (distanceFeet - 400) * 0.02
+    } else if(distanceFeet > 2000 && distanceFeet <=2500){
+        return 25
+    } else{
+        return `cannot travel that far`
+    }
+}
